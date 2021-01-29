8ac2ee10949e60859866b4e3f70c0db65fa76433
Xiaoqingtai <XiaoQT233@163.com>
2021-01-29 11:14:16 +0800

    建立二分查找数字

---
+++ 二分查找.java
@@ -0,0 +1,16 @@
+public static int binarySearch(int[] array,int key) {
+    int left = 0;
+    int right = array.length-1;
+    while (left <= right) {
+        count++;
+        int mid = (left+right)/2;
+        if(array[mid] == key) {
+            return mid;
+        }else if(array[mid] < key) {
+            left = mid+1;
+        }else {
+            right = mid-1;
+        }
+    }
+    return -1;
+}
